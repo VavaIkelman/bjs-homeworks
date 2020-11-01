9993201896fb216970e202020a2b55fbeb969600
VavaIkelman <mr.ilkevich@gmai.com>
2020-11-01 12:28:29 +0300

    Выполнено ДЗ 1.1. Есть ошибка по тесту Жасмин на 2 задание

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -4,24 +4,36 @@
 	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
+ let  a;
+ let  g;
+ let  p;
+ let resultTask1;
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+    resultTask1 = (a + g + p) / 3;
     
-	return // ваш ответ;
+	return resultTask1;
 }
 
+let myName ;
+let message;
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
+	myName = userName;
+    message = `Привет, мир! Меня зовут ${myName}.`;
     
-	return // ваш ответ;
+	return message;
 }
 
-function calculateFormula() {
+let x ;
+let y ;
+let z ;
+let resultTask3
+function calculateFormula(x, y, z) {
+	x = 2;
+    y = 22;
+    z = 0;
+	resultTask3 = (x * y + 5 * z + x - 1) ;
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    return resultTask3
 }
