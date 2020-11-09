ccb91114823ac4a68eb391715c071197897b8ee7
VavaIkelman <mr.ilkevich@gmai.com>
2020-11-10 00:00:24 +0300

    Домашняя работа 2.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,30 @@
+"use strict"
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x, D = Math.pow(b,2) - 4 * a * c;
+if (D < 0) {
+    x = [];
+}
+else if (D == 0) {
+    x = [(-b - Math.sqrt(D)) / 2 * a];
+}
+else if (D > 0) {
+    x = [ (-b + Math.sqrt(D)) / 2 * a , (-b - Math.sqrt(D)) / 2 * a];
+}
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let currentMarks = marks;
+    let n = marks.length;
+    if (n == 0) {
+        averageMark = 0;
+    }
+    else if (n >= 5) {
+      let actualMarks =  marks.slice(0, 5);
+        averageMark = (actualMarks.reduce(0, +)) / n;
+      }
+   
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
