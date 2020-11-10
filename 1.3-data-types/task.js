5115a6a631ad96f3bfcb98d54f68cca36e9cc7b2
VavaIkelman <mr.ilkevich@gmai.com>
2020-11-10 22:18:23 +0300

    Выполненно 1.3.1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,14 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let p = percent, c = contribution, a = amount, d = date;
+    let S = amount - contribution; //тело кредита
+    let P = (p / 100) / 12; // 1/12 процентной ставки(от 0 до 1)
+    let now = new Date(); //текущая дата
+    let n = (d.getFullYear() - now.getFullYear()) * 12 + (d.getMonth() - now.getMonth() - 1);
+    let everyMonthPay = S * (P + P / (((1 + P) ** n) - 1));//ежемесячный платеж
+         totalAmount = everyMonthPay * n;//итоговая выплата
+    let percentOfCredit = totalAmount - S//проценты за пользование кредитом итог
+    
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
