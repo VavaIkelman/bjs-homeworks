b6feda21d547c8537c94b6d64739bb897b51932c
VavaIkelman <mr.ilkevich@gmai.com>
2020-11-10 22:54:24 +0300

    Выполненно 1.3.2

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,19 +1,24 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let p = percent, c = contribution, a = amount, d = date;
-    let S = amount - contribution; //тело кредита
-    let P = (p / 100) / 12; // 1/12 процентной ставки(от 0 до 1)
-    let now = new Date(); //текущая дата
-    let n = (d.getFullYear() - now.getFullYear()) * 12 + (d.getMonth() - now.getMonth() - 1);
-    let everyMonthPay = S * (P + P / (((1 + P) ** n) - 1));//ежемесячный платеж
-         totalAmount = everyMonthPay * n;//итоговая выплата
-    let percentOfCredit = totalAmount - S//проценты за пользование кредитом итог
-    
+    let S = amount - contribution;
+    let P = (p / 12) / 100; 
+    let now = new Date(); 
+    let n = (d.getFullYear() - now.getFullYear()) * 12 + d.getMonth() - now.getMonth();
+    let everyMonthPay = S * (P + P / (((1 + P) ** n) - 1));
+    let  totalAmount = everyMonthPay * n;
+    let percentOfCredit = totalAmount - S;
+ 
     return totalAmount.toFixed(2);
-
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let user = name;
+    let greeting = undefined;
+    if (user == null || user == undefined || user == 0){
+        greeting = "Привет, мир! Меня зовут Аноним"
+    } else {
+        greeting = `Привет, мир! Меня зовут ${user}`
+    }
+    return greeting;
 }
