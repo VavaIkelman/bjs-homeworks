1a205ce11c0c4a39c27d3b2359e4652c8592d7eb
VavaIkelman <mr.ilkevich@gmail.com>
2020-11-10 17:14:25 +0300

    Убрал перенос строки для 1.3 чтобы свериться с жасмин

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -39,7 +39,6 @@ function askDrink(name,dateOfBirthday){
     let userDate = dateOfBirthday;
     let year = new Date().getFullYear();
     let userYear = dateOfBirthday.getFullYear();
-    let result = ((year - userYear) > 18) ? `Не желаете ли олд-фэшн, ${userName}?` : `Сожалею, ${userName}, но я не могу продать вам алкоголь. 
-    Зато могу предложить вам замечательный клюквенный компот!` ;
+    let result = ((year - userYear) > 18) ? `Не желаете ли олд-фэшн, ${userName}?` : `Сожалею, ${userName}, но я не могу продать вам алкоголь. Зато могу предложить вам замечательный клюквенный компот!` ;
     return result;
 }
