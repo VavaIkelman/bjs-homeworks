18d80b4a6444bc89a97d04d659e1d12d9dd3b0f5
VavaIkelman <mr.ilkevich@gmail.com>
2020-11-11 15:56:01 +0300

    Homework 1.2 is done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,10 +1,7 @@
 "use strict"
 function getResult(a,b,c){
-    let x, D = Math.pow(b,2) - 4 * a * c;
-if (D < 0) {
-    x = [];
-}
-else if (D == 0) {
+    let x = [], D = Math.pow(b,2) - 4 * a * c;
+ if (D == 0) {
     x = [(-b - Math.sqrt(D)) / 2 * a];
 }
 else if (D > 0) {
@@ -18,27 +15,22 @@ function getAverageMark(marks){
     let n = marks.length;
     let averageMark ;
     if (n == 0) {
-        averageMark = 0;
+        return 0;
     }
-    else {
-      let actualMarks =  marks.slice(0, 5);{
-        let sumMarks = 0;
-        
-          for (let i = 0; i <= actualMarks.length - 1; i++){
+    let actualMarks =  marks.slice(0, 5);{
+      let sumMarks = 0;
+      for (let i = 0; i < actualMarks.length; i++){
               sumMarks += actualMarks[i];
-              averageMark = sumMarks / actualMarks.length;
-          }
-        } 
       }
-
-    return averageMark;
+      return sumMarks / actualMarks.length;
+    } 
 }
 
 function askDrink(name,dateOfBirthday){
     let userName = name;
     let userDate = dateOfBirthday;
-    let year = new Date().getFullYear();
-    let userYear = dateOfBirthday.getFullYear();
-    let result = ((year - userYear) > 18) ? `Не желаете ли олд-фэшн, ${userName}?` : `Сожалею, ${userName}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!` ;
-    return result;
+    let str1 = `Не желаете ли олд-фэшн, ${userName}?`;
+    let str2 = `Сожалею, ${userName}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    return ((new Date().getFullYear() - userDate.getFullYear()) > 18) ? str1 : str2 ;
+    
 }
