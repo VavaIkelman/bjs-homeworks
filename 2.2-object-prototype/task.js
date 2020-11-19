225c568089bcacefe724a4532362bfa3a3ed1e60
VavaIkelman <mr.ilkevich@gmai.com>
2020-11-19 22:58:31 +0300

    Добавлено выполнение 2.2 :fcvby 5 bp 11

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,16 @@
-//String.prototype.isPalindrome - для задачи №1
+const string = new String();
+
+String.prototype.isPalindrome = function () {
+    let lowerStr = this.toLowerCase();
+    let reversStr = this.split('').reverse().join('');
+    let result = (lowerStr === reversStr);
+    if (result == true){
+        console.log(`Строка ${lowerStr} является полиндромом`);
+    } else {
+        console.log(`Строка ${lowerStr} не является полиндромом`);
+    }
+    return result
+}
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
