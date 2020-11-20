6b7799ed7fcdd7b9fdb885bf6b3cbfe2a1501173
VavaIkelman <mr.ilkevich@gmai.com>
2020-11-20 22:58:58 +0300

    Добавлено выполнение 2.2 Жасмин приняла 10 из 11

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -4,20 +4,30 @@ String.prototype.isPalindrome = function () {
     let strRevers = this.split('').reverse().join('').toLowerCase().replace(/\s/g,'');
     let result = (str === strRevers);
       if (result == true){
-          console.log(`Строка "${this}"" является полиндромом`);
+          console.log(`Строка "${this}" является полиндромом`);
       } else {
-          console.log(`Строка "${this}"" не является полиндромом`);
+          console.log(`Строка "${this}" не является полиндромом`);
       }
       return result
   
   }
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  function getAverageMark(marks) {
+    let average = Array.from(marks);
+    let sum = 0;
+    if (marks == 0 || null || undefined){
+        return 0
+    }
+  for (let i = 0; i < average.length; i++){
+    sum += average[i];
+    }
+   return Math.round(sum / average.length);
 }
 
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let diff = Date.now() - Date.parse(birthday);
+    let age = Math.round(diff / (365.25 * 24 * 60 * 60 * 1000));
+    let verdict = (age > 17) ?  "Да" : "Нет";
+     return verdict
 }
