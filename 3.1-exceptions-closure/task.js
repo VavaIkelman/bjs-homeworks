c6bfcc3d3b5fc6c476aff0a336a4f8b8389690d9
VavaIkelman <mr.ilkevich@gmai.com>
2020-12-07 14:34:59 +0300

    Задане  3.1 выполнено

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,18 +1,18 @@
 function parseCount(x) {
-    if (Number.parseInt(x) !== parseInt(x)) {
-        const e = new Error (`Невалидное значение`);
-        throw e;
-    }
-return Number.parseInt(x)
-}
-
-function validateCount(x) {
-    try {
-        return parseCount(x);
-    } catch (e) {
-         return new Error (`Невалидное значение`);   
-    }    
-}
+    let num = Number.parseInt(x);
+      if (num != x) {
+          throw new Error (`Невалидное значение`);
+      }
+  return num
+  }
+  
+  function validateCount(x) {
+      try {
+          return parseCount(x);
+      } catch (e) {
+            return e
+      }    
+  }
 
 class Triangle{
     constructor(a, b, c) {
@@ -31,15 +31,21 @@ class Triangle{
         let p = (this.getPerimeter() / 2 );
         let s = Math.sqrt( p * (p - this.a) * (p - this.b) * (p - this.c));
         return Number(s.toFixed(3));
-    }
-    
+    }    
 }
 
 function getTriangle(a, b, c) {
     try {
-   return new Triangle(a, b, c);
+        return new Triangle(a, b, c);
       } catch(e) {
-        return `Ошибка! Треугольник не существует`
+        return {
+            getArea() {
+              return 'Ошибка! Треугольник не существует'
+            },
+            getPerimeter() {
+              return 'Ошибка! Треугольник не существует'
+            }
+          }
       }   
 }   
 
